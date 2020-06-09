b35eaff0dc100e774c9c2e69fc5c12c855f60508
Christophe <christophe@MacBook-Pro-de-MBP.local>
2020-06-09 07:17:52 +0300

    Updates

---
+++ precache-manifest.6e16787e28284148cbe0e437a4c0df79.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5a443b601339e98ec064272cc15080f",
+    "url": "/golm/index.html"
+  },
+  {
+    "revision": "ff9d8afd538992276e6d",
+    "url": "/golm/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f10177f385fcfdbea5dc",
+    "url": "/golm/static/js/2.4a269870.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/golm/static/js/2.4a269870.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff9d8afd538992276e6d",
+    "url": "/golm/static/js/main.9f497af7.chunk.js"
+  },
+  {
+    "revision": "5d6902c10b8d1fd687f0",
+    "url": "/golm/static/js/runtime-main.a906ac0b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/golm/static/media/logo.5d5d9eef.svg"
+  }
+]);
